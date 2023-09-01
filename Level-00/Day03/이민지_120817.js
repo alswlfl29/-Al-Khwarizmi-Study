a2105faf5be58619d52b69e47cf201c08189d66c
alswlfl29 <budlalswl1129@gmail.com>
2023-09-01 16:17:35 +0900

    Lv00 3일차 문제풀이

---
+++ Level-00/Day03/이민지_120817.js
@@ -0,0 +1,12 @@
+/* 배열의 평균값
+  문제 설명: 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+*/
+function solution(numbers) {
+  var answer = 0;
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  answer = total / numbers.length;
+  return answer;
+}
