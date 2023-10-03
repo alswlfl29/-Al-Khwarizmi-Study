a001055a8dd2759160965d55d74c3001842f0105
alswlfl29 <budlalswl1129@gmail.com>
2023-10-04 01:32:10 +0900

    기능개발 문제풀이

---
+++ Stack&Queue/이민지_42586.js
@@ -0,0 +1,26 @@
+/**
+ * 기능개발
+ * 문제 설명: 프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+ *          또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+ *          먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+ */
+function solution(progresses, speeds) {
+  let answer = [];
+  while (progresses.length) {
+    count = 0;
+    while (progresses[0] >= 100) {
+      // 맨 앞 요소가 100% 채워지면 꺼내기
+      progresses.shift(); // 맨앞 요소 제거
+      speeds.shift(); // 맨앞 요소 제거
+      count += 1;
+    }
+    if (count > 0) {
+      answer.push(count);
+    }
+    // 각 작업의 속도 더하기
+    progresses = progresses.map((progress, index) => progress + speeds[index]);
+  }
+  return answer;
+}
+
+console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]));
